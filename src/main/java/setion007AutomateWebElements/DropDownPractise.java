b9a396ded115ca5715bec2f54df7bd04fac9bedc
dydko1 <miroslaw.dyduch@gmail.com>
2022-01-09 08:55:21 +0100

    lesson 056 (parent child) finished

--- src/main/java/setion007AutomateWebElements/DropDownPractise.java
+++ src/main/java/setion007AutomateWebElements/DropDownPractise.java
@@ -3,27 +3,38 @@ package main.java.setion007AutomateWebElements;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.testng.Assert;
 
 import java.time.Duration;
+import java.util.List;
 
-public class DropDown {
+public class DropDownPractise {
     public static void main(String[] args) throws InterruptedException {
         String s;
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        driver.get("https://www.spicejet.com/");
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
         driver.manage().window().maximize();
+        driver.findElement(By.id("autosuggest")).sendKeys("ind");
+        Thread.sleep(3000);
+        List<WebElement> options = driver.findElements(By.cssSelector("li.ui-menu-item a"));
 
-        driver.findElement(By.cssSelector("div[data-testid='to-testID-origin']")).click();
-        driver.findElement(By.xpath("//div[@class='css-1dbjc4n r-14lw9ot r-z2wwpe r-1rjd0u6 r-1g94qm0 r-u8s1d r-8fdsdq'] //div[text()='IXA']")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//div[text()='MAA'][1]")).click();
-        Thread.sleep(2000);
+        for (WebElement option : options) {
+            if (option.getText().equalsIgnoreCase("India")) {
+                System.out.println("India found:)");
+            } else {
+                System.out.println("India does not found:(");
+            }
+            ;
+        }
+
+        for (int i = 0; i < options.size(); i++) {
+            System.out.println(options.get(i).getText());
+        }
+
         driver.quit();
     }
 }
