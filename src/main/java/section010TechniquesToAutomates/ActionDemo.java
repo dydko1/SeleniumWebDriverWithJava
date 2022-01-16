96abf1e0d1b8446e768e3f1fd387df8c99209798
dydko1 <miroslaw.dyduch@gmail.com>
2022-01-16 20:31:29 +0100

    lesson 085 finished

--- src/main/java/section010TechniquesToAutomates/ActionDemo.java
+++ src/main/java/section010TechniquesToAutomates/ActionDemo.java
@@ -2,13 +2,16 @@ package main.java.section010TechniquesToAutomates;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.awt.image.Kernel;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
@@ -17,18 +20,19 @@ public class ActionDemo {
     public static void main(String[] args) throws InterruptedException {
         String s = "Mirek";
         ChromeOptions options = new ChromeOptions();
-        //options.addArguments("start-maximized");
+        options.addArguments("start-maximized");
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver(options);
-        WebDriverWait w = new WebDriverWait(driver, Duration.ofSeconds(5));
-
-
+        Actions a = new Actions(driver);
+        //WebDriverWait w = new WebDriverWait(driver, Duration.ofSeconds(5));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get("https://www.amazon.pl/");
-        //driver.findElement(By.cssSelector("#nav-link-accountList")).click();
-        Actions a = new Actions(driver) ;
-        a.moveToElement(driver.findElement(By.cssSelector("#nav-link-accountList"))).build().perform();
+        WebElement move = driver.findElement(By.cssSelector("#nav-link-accountList"));
 
+        // a.moveToElement(driver.findElement(By.cssSelector("#nav-link-accountList"))).build().perform();
+        a.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("hello").doubleClick().build().perform();
+
+        a.moveToElement(move).contextClick().build().perform();
         Thread.sleep(2500);
 
         driver.quit();
