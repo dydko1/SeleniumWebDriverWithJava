c9b658d6ec22344d7bb2c9250f335d7067f935bf
dydko1 <miroslaw.dyduch@gmail.com>
2022-01-17 21:13:10 +0100

    lessson 010 finished

---
+++ src/main/java/section010TechniquesToAutomates/FrameTest.java
@@ -0,0 +1,35 @@
+package main.java.section010TechniquesToAutomates;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.Set;
+
+public class FrameTest {
+    public static void main(String[] args) throws InterruptedException {
+        String s = "Mirek";
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver(options);
+        Actions a = new Actions(driver);
+        //WebDriverWait w = new WebDriverWait(driver, Duration.ofSeconds(5));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://jqueryui.com/droppable/");
+        driver.switchTo().frame(driver.findElement(By.className("demo-frame")));
+        driver.findElement(By.id("draggable")).click();
+        WebElement source = driver.findElement(By.id("draggable"));
+        WebElement destination =driver.findElement(By.id("droppable"));
+        a.dragAndDrop(source,destination).build().perform();
+        Thread.sleep(2000);
+        driver.quit();
+    }
+
+}
